93291178867b733c689bd6ae8218a94efd8b2537
0xabstracted <vskbr23@gmail.com>
2022-05-14 12:34:50 +0530

    Exposing instruction APIs

--- programs/EHR/src/lib.rs
+++ programs/EHR/src/lib.rs
@@ -11,7 +11,31 @@ pub mod error;
 pub mod ehr {
     use super::*;
 
+    pub fn init_bloodtest_booking(ctx: Context<InitBloodtestBooking>) -> Result<()>{
+        instructions::init_bloodtest_booking::handler(ctx)
+    }
+    pub fn init_doctor_profile(ctx: Context<InitDoctorProfile>,  doctor_name: String, doctor_sex: String, doctor_dob: String, doctor_experience_months: u32, doctor_gc_fee: u128, doctor_licence: bool, doctor_gc_expiry_time: i64) -> Result<()>{
+        instructions::init_doctor_profile::handler(ctx, doctor_name, doctor_sex, doctor_dob, doctor_experience_months, doctor_gc_fee, doctor_licence, doctor_gc_expiry_time)
+    }
+    pub fn init_gc_booking(ctx: Context<InitGcBooking>, patient_comments: String) -> Result<()>{
+        instructions::init_gc_booking::handler(ctx, patient_comments)
+    }
     pub fn init_patient_profile(ctx: Context<InitPatientProfile>, name: String, sex: String, dob: String) -> Result<()> {
-       instructions::init_patient_profile::handler(ctx, name, sex, dob)
+        instructions::init_patient_profile::handler(ctx, name, sex, dob)
+     }
+    pub fn init_technician_profile(ctx: Context<InitTechnicianProfile>, technician_name: String, technician_sex: String, technician_dob: String, technician_experience_months: u32, technician_bloodtest_fee: u128, technician_licence: bool, technician_bloodtest_expiry_time: i64) -> Result<()>{
+        instructions::init_technician_profile::handler(ctx, technician_name, technician_sex, technician_dob, technician_experience_months, technician_bloodtest_fee, technician_licence, technician_bloodtest_expiry_time)
+    }
+    pub fn update_bloodtest_report(ctx: Context<UpdateBloodtestReport>,  white_blood_cells: u128, red_blood_cells: u128, blood_platelets: u128, technician_comments: String) -> Result<()>{
+        instructions::update_bloodtest_report::handler(ctx, white_blood_cells, red_blood_cells, blood_platelets, technician_comments)
+    }
+    pub fn update_gc_report(ctx: Context<UpdateGcReport>, blood_pressure: u32, blood_sugar: u32, temperature: u32, doctor_comments: String) -> Result<()>{
+        instructions::update_gc_report::handler(ctx, blood_pressure, blood_sugar, temperature, doctor_comments)
+    }
+    pub fn verify_bloodtest_report(ctx: Context<VerifyBloodtestReport>) -> Result<()>{
+        instructions::verify_bloodtest_report::handler(ctx)
+    }
+    pub fn verify_gc_report(ctx: Context<VerifyGcReport>) -> Result<()>{
+        instructions::verify_gc_report::handler(ctx)
     }
 }
